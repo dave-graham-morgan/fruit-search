eb8fbcf315be83073c74dc0bb94f7ccef0d4f53e
Dave Morgan <dave.g.morgan@gmail.com>
2023-08-22 08:58:09 -0600

    completed fruit search

--- ES2015/js-arrow.js
+++ ES2015/js-arrow.js
@@ -16,13 +16,5 @@ const double = ((arr) => arr.map((val) => val*2));
 //    return evens;
 //  }
 
-// function squareAndFindEvens(numbers){
-//    var squares = numbers.map((num) => num**2);
-//    var evens = squares.filter((square)=>square % 2 ===0);
-//    return evens;
-//  }
 
 const squareAndFindEvens = (numbers) => numbers.map((num) => num**2).filter((square)=>square % 2 ===0);
-
- let numbers = [1,2,3,4,5,6,7,8,9]
- let testFunc = squareAndFindEvens(numbers);
